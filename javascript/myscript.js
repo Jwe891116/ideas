44091e3c85b4c5af46f99c79f10dfe39a0368edb
JWEmmanuel89 <90356423+JWEmmanuel89@users.noreply.github.com>
2025-04-24 15:13:23 -0600

    Possible completion

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -4,26 +4,3 @@ document
   .addEventListener("click", function () {
     document.querySelector(".nav-links").classList.toggle("active");
   });
-
-//About Us Fuctions
-function openTab(tabId) {
-  // Hide all tab contents except Special Forces
-  const tabContents = document.querySelectorAll(
-    ".tab-content:not(.special-forces-content)"
-  );
-  tabContents.forEach((content) => {
-    content.style.display = "none";
-  });
-
-  // Remove active class from all buttons
-  const tabButtons = document.querySelectorAll(".tab-button");
-  tabButtons.forEach((button) => {
-    button.classList.remove("active");
-  });
-
-  // Show the selected tab content
-  document.getElementById(tabId).style.display = "block";
-
-  // Add active class to the clicked button
-  event.currentTarget.classList.add("active");
-}
